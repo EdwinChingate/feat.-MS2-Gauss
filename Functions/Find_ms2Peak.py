8ceff9d0cb0e4bc301028877d20e5f14b6336384
eachingateb <eachingateb@unal.edu.co>
2024-12-07 17:52:13 -0500

    checking changes

--- Functions/Find_ms2Peak.py
+++ Functions/Find_ms2Peak.py
@@ -4,7 +4,7 @@ def Find_ms2Peak(RawSpectrum,mz,mz_std=2e-3,stdDistance=3,minSignals=4,count=0,M
         return []
     min_mz_peak=mz-mz_std*stdDistance
     max_mz_peak=mz+mz_std*stdDistance
-    peakFilter=(RawSpectrum[:,0]>min_mz_peak)&(RawSpectrum[:,0]<max_mz_peak)
+    peakFilter=(RawSpectrum[:,0]>min_mz_peak)&(RawSpectrum[:,0]<max_mz_peak)&(RawSpectrum[:,1]>0)
     PeakData=RawSpectrum[peakFilter,:]            
     if RelativeContribution:
         PeakInt=sum(PeakData[:,1])
