c774c2ddfad2e3f9aacfac780f4b670bc41fc82f
eachingateb <eachingateb@unal.edu.co>
2025-01-09 11:20:38 -0500

    AllPeaks All Chrom

--- Functions/ResolveFullChromatogram.py
+++ Functions/ResolveFullChromatogram.py
@@ -2,8 +2,8 @@ import pandas as pd
 from AllSubChromatograms import *
 from ResolvingGaussianChromatogram import *
 from GaussianParametersTable import *
-def ResolveFullChromatogram(mz,mz_std,stdDistance=3,RT_tol=5,minSignals=5,SavePeaks=True,mz_name='mz'):
-    ChromatogramList=AllSubChromatograms(mz=mz,mz_std=mz_std,stdDistance=stdDistance,RT_tol=RT_tol,minSignals=minSignals)
+def ResolveFullChromatogram(mz,mz_std,AllPeaks,stdDistance=3,RT_tol=5,minSignals=5,SavePeaks=True,mz_name='mz'):
+    ChromatogramList=AllSubChromatograms(mz=mz,mz_std=mz_std,AllPeaks=AllPeaks,stdDistance=stdDistance,RT_tol=RT_tol,minSignals=minSignals)
     GaussianParametersList=[]
     for Chromatogram in ChromatogramList:
         GaussianParameters=ResolvingGaussianChromatogram(Chromatogram=Chromatogram,RT_col=2,int_col=1,MaxSignals=100,distance=2)
